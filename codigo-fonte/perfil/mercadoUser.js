0ed84d1e433d46272a55a9dddde8a0638de6d2bc
Priscila Maruno <myuki.maruno@gmail.com>
2023-11-09 02:00:48 +0900

    Delete mercadoUser.js

--- codigo-fonte/perfil/mercadoUser.js
+++
@@ -1,8 +0,0 @@
-let infoMercado = JSON.parse(localStorage.getItem("infoMercado"));
-document.getElementById("nome").innerHTML = infoMercado.nome;
-document.getElementById("nomeTitulo").innerHTML = infoMercado.nome;
-document.getElementById("email").innerHTML = infoMercado.email;
-document.getElementById("telefone").innerHTML = infoMercado.telefone;
-document.getElementById("instituicao").innerHTML = infoMercado.instituicao;
-document.getElementById("categoria").innerHTML = infoMercado.opcoes;
-document.getElementById("endereco").innerHTML = infoMercado.endereco;
