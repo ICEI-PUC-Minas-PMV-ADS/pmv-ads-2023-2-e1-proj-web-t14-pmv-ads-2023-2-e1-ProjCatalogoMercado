17fcaf9ef8ca1c5f8591943c7cc077227edd2e30
douglasaraujo12 <144749545+douglasaraujo12@users.noreply.github.com>
2023-11-22 17:39:27 -0300

    Add files via upload

---
+++ codigo-fonte/conta.js
@@ -0,0 +1,17 @@
+function Editar conta(){
+            
+    var ususername = document.getElementById('Nome da Empresa').value;
+    var senha = document.getElementById('Senha').value;
+
+    if(email== "admin" && senha == "1234"){
+        alert('sucesso');
+       
+    
+      }
+
+      else{
+            alert('usuario ou senha incorretos');
+
+    } 
+
+}
