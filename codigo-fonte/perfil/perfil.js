c3ec4b5d09cfc75ddf1432bbd405f1979bd09f19
Priscila Maruno <myuki.maruno@gmail.com>
2023-11-26 03:58:31 +0900

    Update perfil.js

--- codigo-fonte/perfil/perfil.js
+++ codigo-fonte/perfil/perfil.js
@@ -1,5 +1,36 @@
+// Mostrar dados escritos no cadastro
 let infoCadastro = JSON.parse(localStorage.getItem('infoMercado'))
-document.getElementById("nomeMenu").innerHTML = infoCadastro.nomeMercado;
+document.getElementById("perfilNome").innerHTML = infoCadastro.nomeMercado;
 document.getElementById("nomeMercado").innerHTML = infoCadastro.nomeMercado;
 document.getElementById("enderecoMercado").innerHTML = infoCadastro.enderecoMercado;
 document.getElementById("telefoneMercado").innerHTML = infoCadastro.telefoneMercado;
+
+// Mostrar produto com os dados do localStorage
+function mostrarProduto () {
+// Carregar os produtos
+let dados = readProduto ();
+
+//gerar conteúdo do produtos
+let conteudoProduto ="";
+dados.forEach((item) => {
+    conteudoProduto += `
+    <tr id= 'linha-${item.id}'>
+        <td class="nomeProduto">
+        ${item.nome}
+        </td>
+        <td class="descricaoProduto">
+        ${item.descricao}
+        </td>
+        <td class="precoProduto">
+        ${item.preco}
+        </td>
+        <td class="classProduto">
+        ${item.class}
+        </td>
+        <td class="imgProduto">
+        ${item.img}
+        </td>
+    `;
+});
+corpoProduto.innerHTML = conteudoProduto;
+}
