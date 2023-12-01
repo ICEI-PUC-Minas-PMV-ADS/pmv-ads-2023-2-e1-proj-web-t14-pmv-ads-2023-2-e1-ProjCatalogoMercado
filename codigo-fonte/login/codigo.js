cc71db45964abe03b94504bed9f2df001f8b8be8
Priscila Maruno <myuki.maruno@gmail.com>
2023-12-02 08:42:39 +0900

    Update codigo.js

--- codigo-fonte/login/codigo.js
+++ codigo-fonte/login/codigo.js
@@ -3,7 +3,7 @@ function logar(){
     var email = document.getElementById('E-mail').value;
     var senha = document.getElementById('Senha').value;
 
-    if(email== "admin" && senha == "1234"){
+    if(email== "" && senha == ""){
         alert('sucesso');
     
       }
